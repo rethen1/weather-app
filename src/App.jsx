ae9adc0d9d8b4997ef817498df3c60f3742693c2
rethen1 <rethen903@gmail.com>
2025-04-17 15:56:45 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Weather from 'src/components/Weather'
+import Weather from './components/Weather'
 
 const App = () => {
   return (
